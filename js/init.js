4c23f4d8e2b4b78c084de5e2aa812a599ee44466
mastercoks <matheusbdo1@gmail.com>
2017-08-30 09:53:34 -0300

    novo site

---
+++ js/init.js
@@ -0,0 +1,26 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+var ativado = false;
+function show_search() {
+  if (!ativado) {
+    document.getElementById('search-bar').className = "ativado";
+    document.getElementById('navbar-menu').className = "hide";
+    document.getElementById("label_search").focus();
+    ativado = true;
+  } else {
+    document.getElementById('search-bar').className = "hide desativado";
+    document.getElementById('navbar-menu').className = "";
+    ativado = false;
+  }
+}
+function hide_search() {
+  document.getElementById('search-bar').className = "hide";
+  document.getElementById('navbar-menu').className = "";
+  ativado = false;
+}
